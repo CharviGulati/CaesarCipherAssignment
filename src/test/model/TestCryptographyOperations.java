9dbd2e71f2e690cf18311f83e31ab52b4ce3685e
gulati <cgulati@student.ubc.ca>
2021-02-13 22:00:29 -0800

    worked on TestCryptographyOperations class

---
+++ src/test/model/TestCryptographyOperations.java
@@ -0,0 +1,40 @@
+package model;
+
+import encryption.CryptographyOperation;
+import java.util.Date;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCryptographyOperations {
+
+    @Test
+    public void testMutators() {
+        CryptographyOperation cryptographyOperation = new CryptographyOperation("encryption", new Date(), "cipherText",
+                "plainText", 0, 000);
+
+        assertEquals("encryption", cryptographyOperation.getType());
+        assertEquals("cipherText", cryptographyOperation.getCiphertext());
+        assertEquals("plainText", cryptographyOperation.getPlaintext());
+        assertEquals(0, cryptographyOperation.getKey());
+        assertEquals(000, cryptographyOperation.getId());
+
+
+        cryptographyOperation.setType("decryption");
+        assertEquals("decryption", cryptographyOperation.getType());
+
+        cryptographyOperation.setCiphertext("newCipherText");
+        assertEquals("newCipherText", cryptographyOperation.getCiphertext());
+
+        cryptographyOperation.setPlaintext("newPlainText");
+        assertEquals("newPlainText", cryptographyOperation.getPlaintext());
+
+        cryptographyOperation.setKey(5);
+        assertEquals(5, cryptographyOperation.getKey());
+
+        cryptographyOperation.setId(7);
+        assertEquals(7, cryptographyOperation.getId());
+
+    }
+
+
+}
