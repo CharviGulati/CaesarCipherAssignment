920557d8dadf55e38457574a66f096ba44e6a17c
gulati <cgulati@student.ubc.ca>
2021-03-21 18:30:48 -0700

    Almost done

--- src/main/model/CryptographyOperationsList.java
+++ src/main/model/CryptographyOperationsList.java
@@ -34,6 +34,9 @@ public class CryptographyOperationsList implements Writable {
         this.cryptographyOperations.removeIf(cryptographyOperation -> cryptographyOperation.getId() == id);
     }
 
+    public void removeOperationByIndex(int index) {
+        this.cryptographyOperations.remove(index);
+    }
 
     // REQUIRES: ArrayList<CryptographyOperation> cryptographyOperations to not be empty
     // EFFECTS: filters the array list to find only EncryptionOperations
--- src/main/ui/CaesarCipherScreen.java
+++ src/main/ui/CaesarCipherScreen.java
@@ -3,27 +3,33 @@ package ui;
 import model.CaesarCipher;
 import model.CryptographyOperation;
 import model.CryptographyOperationsList;
+import persistence.JsonReader;
+import persistence.JsonWriter;
 
 import javax.swing.*;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Date;
 
 public class CaesarCipherScreen extends JFrame {
-    private JTextField cryptographyInput;
     private JButton encryptionButton;
     private JButton saveOperationButton;
     private JButton removeFromFileButton;
-    private JButton seePreviousEncryptionOperationsButton;
-    private JButton seePreviousDecryptionOperationsButton;
+    private JButton decryptButton;
+    private JButton loadPreviousCryptographyOps;
+    private JTextField cryptographyInput;
     private JPanel panelMain;
     private JTextField keyInput;
     private JList cryptographyOperationsJList;
-    private JButton decryptButton;
     private CryptographyOperationsList cryptographyOperationsList;
     private DefaultListModel defaultListCryptOps;
+    private static JsonWriter writer = new JsonWriter("./data/CaesarCipher.txt");
+    private static JsonReader jsonReader = new JsonReader("./data/CaesarCipher.txt");
+    private int selectedCryptographyOperationIndex;
 
 
     public CaesarCipherScreen() {
@@ -33,16 +39,15 @@ public class CaesarCipherScreen extends JFrame {
         this.pack();
         cryptographyOperationsList = new CryptographyOperationsList();
 
-        // link the screen list with cryptographyList
+        // links the screen list with cryptographyList
         defaultListCryptOps = new DefaultListModel();
         cryptographyOperationsJList.setModel(defaultListCryptOps);
 
-        // setting buttons to be disabled at the start until cryptographyOps are saved
-        // set them to be true in the operation performed
         removeFromFileButton.setEnabled(false);
         saveOperationButton.setEnabled(false);
         encryptionButton.setEnabled(true);
         decryptButton.setEnabled(true);
+        loadPreviousCryptographyOps.setEnabled(true);
 
         setVisible(true);
 
@@ -100,51 +105,64 @@ public class CaesarCipherScreen extends JFrame {
         saveOperationButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                try {
+                    writer.open();
+                } catch (FileNotFoundException fileNotFoundException) {
+                    fileNotFoundException.printStackTrace();
+                }
+                writer.write(cryptographyOperationsList);
+                writer.close();
 
+                removeFromFileButton.setEnabled(true);
             }
         });
 
         removeFromFileButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-//                System.out.println("These are all your entries, with their specific ID number.\n");
-//                printModifiedEncryptionOperations();
-//                printModifiedDecryptionOperations();
-//                System.out.println("Type the ID of the entry you would like to remove: \n");
-//                int userIDInput = scanner.nextInt();
-//                cryptographyOperationsList.removeOperation(userIDInput);
-//                System.out.println("Entry with the ID " + userIDInput + " has been deleted from file\n\n");
+                if (selectedCryptographyOperationIndex >= 0) {
+                    cryptographyOperationsList.getCryptographyOperations().remove(selectedCryptographyOperationIndex);
+                    try {
+                        cryptographyOperationsList = jsonReader.read();
+                        cryptographyOperationsList.removeOperationByIndex(selectedCryptographyOperationIndex);
+                        refreshCryptographyOperationsList();
 
+                        writer.open();
+                        writer.write(cryptographyOperationsList);
+                        writer.close();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                    refreshCryptographyOperationsList();
+                    if (cryptographyOperationsList.getCryptographyOperations().size() == 0) {
+                        removeFromFileButton.setEnabled(false);
+                    }
+                }
             }
         });
 
-        seePreviousEncryptionOperationsButton.addActionListener(new ActionListener() {
+        loadPreviousCryptographyOps.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
-            }
-        });
-
-        seePreviousDecryptionOperationsButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
+                try {
+                    cryptographyOperationsList = jsonReader.read();
+                    refreshCryptographyOperationsList();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
         });
 
         cryptographyOperationsJList.addListSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
-
+                int index = cryptographyOperationsJList.getSelectedIndex();
+                if (index != -1) {
+                    selectedCryptographyOperationIndex = index;
+                }
             }
         });
 
-        cryptographyOperationsJList.addListSelectionListener(new ListSelectionListener() {
-            @Override
-            public void valueChanged(ListSelectionEvent e) {
-
-            }
-        });
     }
 
     public void refreshCryptographyOperationsList() {
@@ -153,36 +171,22 @@ public class CaesarCipherScreen extends JFrame {
 
         for (CryptographyOperation cryptographyOperation : cryptographyOperationsList.getCryptographyOperations()) {
             if (cryptographyOperation.getType().equalsIgnoreCase("Caesar Cipher Encryption")) {
-                operationDescription = "The text you entered is " + cryptographyOperation.getPlaintext()
-                        + " with the key " + cryptographyOperation.getKey()
-                        + " your cipher text is >>> " + cryptographyOperation.getCiphertext();
+                operationDescription = "NEW ENCRYPTION OPERATION: " + "'" + cryptographyOperation.getPlaintext() + "'"
+                        + " with the key " + "'" + cryptographyOperation.getKey() + "'"
+                        + " your cipher text is >>> " + cryptographyOperation.getCiphertext()
+                        + " with the specific ID of: " + cryptographyOperation.getId();
                 defaultListCryptOps.addElement(operationDescription);
             } else if (cryptographyOperation.getType().equalsIgnoreCase("Caesar Cipher Decryption")) {
-                operationDescription = "The text you entered is " + cryptographyOperation.getCiphertext()
-                        + " with the key " + cryptographyOperation.getKey()
-                        + " your cipher text is >>> " + cryptographyOperation.getPlaintext();
+                operationDescription = "NEW DECRYPTION OPERATION: " + "'" + cryptographyOperation.getCiphertext() + "'"
+                        + " with the key " + "'" + cryptographyOperation.getKey() + "'"
+                        + " your plain text is >>> " + cryptographyOperation.getPlaintext()
+                        + " with the specific ID of: " + cryptographyOperation.getId();
                 defaultListCryptOps.addElement(operationDescription);
             }
         }
+        if (cryptographyOperationsList.getCryptographyOperations().size() >= 1) {
+            saveOperationButton.setEnabled(true);
+        }
     }
-
-//    public void printModifiedEncryptionOperations() {
-//        System.out.println("\nEncryption Operations currently recorded: ");
-//        if (cryptographyOperationsList.getEncryptionOperations().size() == 0) {
-//            System.out.println("\nNo Encryption operations recorded\n");
-//            System.out.print("\n-----------------------------------------------------------------\n");
-//        } else {
-//            for (CryptographyOperation encryptionOperation : cryptographyOperationsList.getEncryptionOperations()) {
-//                System.out.println(encryptionOperation.getType()
-//                        + ": You entered the cipher text: "
-//                        + encryptionOperation.getPlaintext()
-//                        + " that was given the ID: "
-//                        + encryptionOperation.getId());
-//            }
-//        }
-//    }
-
-
-// user types in value, button, get value
 
 }
